ada3711bc3548c638cf501d501b6b083f504d001
hoon2hooni <sanghoon.engineer@gmail.com>
2023-01-09 10:15:50 +0900

    refac: remove redundant files and packages

--- styles/theme.ts
+++
@@ -1,9 +0,0 @@
-import type { Theme } from "@emotion/react";
-
-const theme: Theme = {
-  colors: {
-    primary: "hotpink",
-  },
-};
-
-export default theme;
--- types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NEXT_PUBLIC_END_POINT: string;
-    }
-  }
-}
-
-export {};
