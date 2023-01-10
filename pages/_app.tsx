e431b74336f27339073709ef8f3a35abb7de13ee
hoon2hooni <sanghoon.engineer@gmail.com>
2023-01-10 14:37:23 +0900

    imp: add favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useState } from "react";
-
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => {
     return new QueryClient({
@@ -15,11 +15,17 @@ export default function App({ Component, pageProps }: AppProps) {
     });
   });
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-      <ReactQueryDevtools initialIsOpen={false} position={"top-left"} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/search-problem.png" />
+        <title>show me the issues</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+        <ReactQueryDevtools initialIsOpen={false} position={"top-left"} />
+      </QueryClientProvider>
+    </>
   );
 }
